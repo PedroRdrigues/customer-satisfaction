b7b4b55f0dc792c83f10e81b886be7e376c8037d
ZorcKs <p.hrdrigues1@gmail.com>
2024-04-19 23:20:28 -0300

    Commit inicial

---
+++ App/automation/auto.py
@@ -0,0 +1,5 @@
+# Criar uma automação utilizando o SELENIUM para fazer o download do arquivo .zip contendo o arquivo da planilha
+
+# Mover o arquivo .zip da pasta downloads para a pasta "automation"
+
+# Criar uma automação que faça um relatório e envie e-mails para o dono do estabelecimento
