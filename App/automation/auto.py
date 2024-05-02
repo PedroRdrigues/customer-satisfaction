fe264885718682b70dd1a3aae0fab7af69226bc6
ZorcKs <p.hrdrigues1@gmail.com>
2024-05-01 23:44:35 -0300

    Adaptação do código para usar .xlsx no lugar de .csv

--- App/automation/auto.py
+++ App/automation/auto.py
@@ -1,5 +1,4 @@
-# Criar uma automação utilizando o SELENIUM para fazer o download do arquivo .zip contendo o arquivo da planilha
-
-# Mover o arquivo .zip da pasta downloads para a pasta "automation"
-
 # Criar uma automação que faça um relatório e envie e-mails para o dono do estabelecimento
+def hello(str):
+  print(str)
+
